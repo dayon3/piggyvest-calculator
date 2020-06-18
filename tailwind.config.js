072019cc004ed4c4e2c944fcf5c5fc4906575381
dayon3 <aliasdayone@gmail.com>
2020-06-18 16:20:50 +0300

    add homepage

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	purge: [],
+	theme: {
+		extend: {
+			colors: {
+				'primary-blue': '#144499',
+			},
+		},
+		fontFamily: {
+			display: ['Josefin Sans', 'sans-serif'],
+		},
+	},
+	variants: {},
+	plugins: [],
+};
