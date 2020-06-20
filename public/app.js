a94e9dd8a2931a74130db868d6a54ef32c949778
dayon3 <aliasdayone@gmail.com>
2020-06-20 11:58:30 +0300

    fix number format function

--- public/app.js
+++ public/app.js
@@ -20,6 +20,10 @@ let UIController = (() => {
 		calculateBtn: '.calculate',
 	};
 
+	let numberWithCommas = (integer) => {
+		return integer.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+	};
+
 	let formatNumber = (num) => {
 		let numSplit, int, dec;
 
@@ -32,12 +36,9 @@ let UIController = (() => {
 		numSplit = num.split('.');
 
 		int = numSplit[0];
-		if (int.length > 3) {
-			int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);
-		}
-
 		dec = numSplit[1];
-		return int + '.' + dec;
+
+		return numberWithCommas(int) + '.' + dec;
 	};
 
 	return {
